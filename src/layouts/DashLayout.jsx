e081182f909e5a2845584b522a184e0089081874
HAMIDREZA HASHEMI <HH7192@gmail.com>
2022-07-22 18:33:27 +0430

    3 layouts.

--- src/layouts/DashLayout.jsx
+++ src/layouts/DashLayout.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import {Outlet} from "react-router-dom";
+import Sidebar from "../components/_dash/Sidebar";
 
 const DashLayout = () => {
-  return <div>DashLayout</div>;
+  return (
+    <>
+      <Sidebar />
+      <Outlet />
+    </>
+  );
 };
 
 export default DashLayout;
--- src/layouts/WebLayout.jsx
+++ src/layouts/WebLayout.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import {Outlet} from "react-router-dom";
+import Navbar from "../components/_web/Navbar";
+import Footer from "../components/_web/Footer";
 
 const WebLayout = () => {
-  return <div>WebLayout</div>;
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
 };
 
 export default WebLayout;
