4d9dd5ef6a676b005e3d41e75824d401ba191022
HAMIDREZA HASHEMI <HH7192@gmail.com>
2022-07-22 18:27:57 +0430

    2 routes.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+import WebLayout from "./layouts/WebLayout";
+import Blogs from "./pages/_web/Blogs";
+import Home from "./pages/_web/Home";
+import Login from "./pages/_web/Login";
+import Signup from "./pages/_web/Signup";
+import SingleBlog from "./pages/_web/SingleBlog";
+import SingleUser from "./pages/_web/SingleUser";
+import Users from "./pages/_web/Users";
+
+import DashLayout from "./layouts/DashLayout";
+import PostBlog from "./pages/_dash/PostBlog";
+import EditProfile from "./pages/_dash/EditProfile";
+import EditBlog from "./pages/_dash/EditBlog";
+import MyBlogs from "./pages/_dash/MyBlogs";
+
 function App() {
-  return <div>Blogtor</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<WebLayout />}>
+          <Route path='/' element={<Home />} />
+          <Route path='blogs' element={<Blogs />} />
+          <Route path='blogs/:id' element={<SingleBlog />} />
+          <Route path='users' element={<Users />} />
+          <Route path='users/:id' element={<SingleUser />} />
+          <Route path='login' element={<Login />} />
+          <Route path='signup' element={<Signup />} />
+        </Route>
+
+        <Route path='/dashboard/' element={<DashLayout />}>
+          <Route path='write' element={<PostBlog />} />
+          <Route path='profile' element={<EditProfile />} />
+          <Route path='editblog' element={<EditBlog />} />
+          <Route path='myblogs' element={<MyBlogs />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
