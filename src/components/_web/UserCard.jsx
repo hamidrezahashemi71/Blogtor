ee804e4ea4845874c11b03b7cd9e0a26f7805091
HAMIDREZA HASHEMI <HH7192@gmail.com>
2022-07-22 23:32:00 +0430

    12 ui: user card

--- src/components/_web/UserCard.jsx
+++ src/components/_web/UserCard.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import {Link} from "react-router-dom";
 
 const UserCard = () => {
-  return <div>UserCard</div>;
+  return (
+    <Link to={"/users/:id"}>
+      <div className='rounded-3xl overflow-hidden shadow-xl max-w-xs my-3 mx-2 bg-blue-500'>
+        <img src='https://i.imgur.com/dYcYQ7E.png' className='w-full' />
+        <div className='flex justify-center -mt-8'>
+          <img
+            src={"assets/images/user.png"}
+            className='rounded-full border-solid border-white border-2 -mt-3 h-24'
+          />
+        </div>
+        <div className='text-center px-3 pb-6 pt-2'>
+          <h3 className='text-white text-sm bold font-sans'>Olivia Dunham</h3>
+          <p className='mt-2 font-sans font-light text-white'>
+            Hello, i'm from another the other side!
+          </p>
+        </div>
+        <div className='flex justify-center pb-3 text-white'>
+          <div className='text-center mr-3 border-r pr-3'>
+            <h2>5</h2>
+            <span>Blogs</span>
+          </div>
+          <div className='text-center'>
+            <h2>3.5</h2>
+            <span>Rate</span>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
 };
 
 export default UserCard;
--- src/pages/_web/Users.jsx
+++ src/pages/_web/Users.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import UserCard from "../../components/_web/UserCard";
 
 const Users = () => {
-  return <div>Users</div>;
+  return (
+    <div className='flex flex-wrap p-10 mx-auto '>
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+    </div>
+  );
 };
 
 export default Users;
+ 
