0f4a896a2f2270a362db9053bc3505978cacc210
HAMIDREZA HASHEMI <HH7192@gmail.com>
2022-07-22 18:53:41 +0430

    4 currentUser redux.

---
+++ src/features/Slice.js
@@ -0,0 +1,17 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+export const CurrentUserSlice = createSlice({
+  name: "blogtor",
+  initialState: {
+    currentUser: {},
+  },
+  reducers: {
+    setCurrentUser: (state, action) => {
+      if (action.payload._id) state.currentUser = action.payload;
+    },
+  },
+});
+
+export const {setCurrentUser: setCurrentUser} = CurrentUserSlice.actions;
+export const userSelect = (state) => state.reducer.thisUser;
+export default CurrentUserSlice.reducer;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import Store from "./features/Store";
+import {Provider} from "react-redux";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={Store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
