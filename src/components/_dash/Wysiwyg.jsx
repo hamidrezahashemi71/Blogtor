b144f6e130d9a8a2becd8425edd228587ef86669
HAMIDREZA HASHEMI <HH7192@gmail.com>
2022-07-23 09:03:25 +0430

    14 ui: post blog.

--- src/components/_dash/Wysiwyg.jsx
+++ src/components/_dash/Wysiwyg.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import {Editor} from "@tinymce/tinymce-react";
 
-const Wysiwyg = () => {
-  return <div>Wysiwyg</div>;
+const Wysiwyg = (props) => {
+  return (
+    <div className='mt-4'>
+      <Editor
+        onInit={(evt, editor) => (props.editorRef.current = editor)}
+        initialValue='<p>This is the initial content of the editor.</p>'
+        init={{
+          height: 500,
+          menubar: false,
+          toolbar:
+            "undo redo | formatselect | " +
+            "bold italic backcolor | alignleft aligncenter " +
+            "alignright alignjustify | bullist numlist outdent indent | " +
+            "removeformat | help",
+          content_style:
+            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+        }}
+      />
+    </div>
+  );
 };
 
 export default Wysiwyg;
