627e8e52e00f7589bdbaf14cd303643615d0282c
HAMIDREZA HASHEMI <HH7192@gmail.com>
2022-07-23 09:50:18 +0430

    16 ui: edit profile.

--- src/layouts/DashLayout.jsx
+++ src/layouts/DashLayout.jsx
@@ -3,10 +3,10 @@ import Sidebar from "../components/_dash/Sidebar";
 
 const DashLayout = () => {
   return (
-    <>
+    <div className='bg-blue-100'>
       <Sidebar />
       <Outlet />
-    </>
+    </div>
   );
 };
 
