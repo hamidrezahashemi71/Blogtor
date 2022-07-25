a975b64bd3370ade01cb660b574f2466ad610dc2
HAMIDREZA HASHEMI <HH7192@gmail.com>
2022-07-25 14:57:04 +0430

    single blog template and rating.

--- src/components/_web/BlogCard.jsx
+++ src/components/_web/BlogCard.jsx
@@ -4,42 +4,49 @@ const BlogCard = ({blog}) => {
   return (
     <div className='max-w-lg mx-3'>
       <div className='bg-white shadow-md border border-gray-200 rounded-lg max-w-sm mb-5'>
-        <Link to='#'>
-          <img
-            className='rounded-t-lg'
-            src='https://flowbite.com/docs/images/blog/image-1.jpg'
-            alt=''
-          />
-        </Link>
+        <img
+          className='rounded-t-lg'
+          src={
+            blog.imgurl === "" ? "/assets/images/blog-image.webp" : blog.imgurl
+          }
+          alt=''
+        />
         <div className='p-5'>
-          <Link to='#'>
-            <h5 className='text-gray-900 font-bold text-2xl tracking-tight mb-2'>
-              Noteworthy technology acquisitions 2021
-            </h5>
-          </Link>
+          <h5 className='text-gray-900 font-bold text-2xl tracking-tight mb-2'>
+            {blog.title}
+          </h5>
+
           <p className='font-normal text-gray-700 mb-3'>
             Here are the biggest enterprise technology acquisitions of 2021 so
             far, in reverse chronological order.
           </p>
           <div className='flex justify-between'>
-            <Link to='#!'>
-              <div className='flex items-center'>
-                <div className='shrink-0'>
-                  <img
-                    src={"assets/images/user.png"}
-                    className='rounded-full w-10'
-                    alt='Avatar'
-                  />
+            {blog.creator ? (
+              <Link to={`/users/${blog.creator._id}`}>
+                <div className='flex items-center'>
+                  <div className='shrink-0'>
+                    <img
+                      src={
+                        blog.creator.avatar === ""
+                          ? "/assets/images/user.png"
+                          : blog.creator.avatar
+                      }
+                      className='rounded-full w-10'
+                      alt='Avatar'
+                    />
+                  </div>
+                  <div className='grow ml-3'>
+                    <p className='text-sm font-semibold text-blue-600'>
+                      {blog.creator.name}
+                    </p>
+                  </div>
                 </div>
-                <div className='grow ml-3'>
-                  <p className='text-sm font-semibold text-blue-600'>
-                    Jason McCoel
-                  </p>
-                </div>
-              </div>
-            </Link>
+              </Link>
+            ) : (
+              ""
+            )}
             <Link
-              to={"/blogs/:id"}
+              to={`/blogs/${blog._id}`}
               className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center'>
               Read more
             </Link>
